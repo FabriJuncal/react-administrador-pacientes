f7ea5ca86b86ede3ad38ea41dba3827c567c1f0a
FabriJuncal <juncalfabri@gmail.com>
2021-02-13 15:39:01 -0300

    61. Mostrando las Citas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from "react";
 import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
 
@@ -25,7 +26,13 @@ function App() {
                   />
                 </div>
                 <div className="one-half column">
-
+                  <h2>Administra tus citas</h2>
+                  {citas.map(cita => (
+                    <Cita
+                      key={cita.key}
+                      cita={cita}
+                    />
+                  ))}
                 </div>
               </div>
             </div>
