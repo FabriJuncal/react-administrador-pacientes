c4062d1a507032d5129bbbf416ec326d8aa1628c
fjuncal <fjuncal@tdi.com.ar>
2021-02-13 15:09:03 -0300

    60. Reiniciar el Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -12,6 +12,7 @@ const Formulario = ({crearCita}) => {
         sintomas: ''
     });
 
+    // Creamos el State para los Errores
     const [error, actualizarError] = useState(false);
 
     // Función que se ejecuta cada vez que el usuario escribe en el input
@@ -35,7 +36,7 @@ const Formulario = ({crearCita}) => {
     const submitCita = e => {
         e.preventDefault();
 
-        // Validar 
+        // Validamos que ningun campo este vacio
         //.trim(): Eliminas los espacios en blancos del comienzo y del final de un string
         if(mascota.trim() === '' || propietario.trim() === '' || fecha.trim() === '' || hora.trim() === '' || sintomas.trim() === ''){
             // Actualizamos el State de Errores a "true" en el caso que se envie un campo vacio
@@ -43,25 +44,38 @@ const Formulario = ({crearCita}) => {
             return
         }
 
-        // En el caso que pase la validación volvemos a actualizar el state de errores a "false" para que no se siga mostrando el mensaje de error
+        // En el caso que pase la validación reseteamos el state de errores a "false" para que no se siga mostrando el mensaje de error
         actualizarError(false);
 
-        // Generamos un ID con la libreria 
+        // Generamos un ID con la libreria "uuid"
         cita.id = uuid();
 
         // Creamos la cita
         // Utilizamos la función que se envió por parametro (o props) desde el componente padre para crear las Citas
+        // Este ejecutará la función "guardarCitas" para actualizar el State de Citas
         crearCita(cita);
+
+        // Reseteamos el Formulario
+        actualizarCita({
+            mascota:  '',
+            propietario: '',
+            fecha: '',
+            hora: '',
+            sintomas: ''
+        });
 
     }
 
     return ( 
         <Fragment>
             <h2>Crear Citas</h2>
+            {/* Dentro del "return" solo se pueden agregar las condicionales con "Operadores Ternarios"*/}
             {/* Mostramos un mensaje de error en el caso que el State de errores se actualice a "true" */}
             {error ? <p className="alerta-error">Todos los campos son obligatorios</p> : null}
 
             <form
+                // Agregamos la función "submitCita" al evento "onSubmit" del formulario
+                // Cuando se envie el formulario, se ejecutará la función "submitCita"
                 onSubmit={submitCita}
             >
                 <label>Nombre Mascota</label>
